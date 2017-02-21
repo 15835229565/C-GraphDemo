5ad53df3f7b25f2cb63670930ab4cc518458cb06
easp13 <easp13@gmail.com>
2017-02-21 15:24:40 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿using DmitryBrant.CustomControls;
+
+namespace VectonSensingDemo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.zed1 = new ZedGraph.ZedGraphControl();
+            this.ssData = new DmitryBrant.CustomControls.SevenSegmentArray();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.Color.Khaki;
+            this.btnPause.Location = new System.Drawing.Point(270, 16);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(88, 41);
+            this.btnPause.TabIndex = 181;
+            this.btnPause.Text = "&Pause";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.NavajoWhite;
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(139, 15);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(88, 41);
+            this.btnStop.TabIndex = 180;
+            this.btnStop.Text = "&Stop";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnStart.Location = new System.Drawing.Point(11, 15);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(99, 41);
+            this.btnStart.TabIndex = 179;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // zed1
+            // 
+            this.zed1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.zed1.EditButtons = System.Windows.Forms.MouseButtons.Left;
+            this.zed1.Location = new System.Drawing.Point(12, 101);
+            this.zed1.Name = "zed1";
+            this.zed1.PanModifierKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Shift | System.Windows.Forms.Keys.None)));
+            this.zed1.ScrollGrace = 0D;
+            this.zed1.ScrollMaxX = 0D;
+            this.zed1.ScrollMaxY = 0D;
+            this.zed1.ScrollMaxY2 = 0D;
+            this.zed1.ScrollMinX = 0D;
+            this.zed1.ScrollMinY = 0D;
+            this.zed1.ScrollMinY2 = 0D;
+            this.zed1.Size = new System.Drawing.Size(636, 461);
+            this.zed1.TabIndex = 182;
+            // 
+            // ssData
+            // 
+            this.ssData.ArrayCount = 4;
+            this.ssData.ColorBackground = System.Drawing.Color.Black;
+            this.ssData.ColorDark = System.Drawing.Color.FromArgb(((int)(((byte)(80)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ssData.ColorLight = System.Drawing.Color.Red;
+            this.ssData.DecimalShow = true;
+            this.ssData.ElementPadding = new System.Windows.Forms.Padding(6, 4, 4, 4);
+            this.ssData.ElementWidth = 12;
+            this.ssData.ItalicFactor = -0.1F;
+            this.ssData.Location = new System.Drawing.Point(415, 25);
+            this.ssData.Name = "ssData";
+            this.ssData.Size = new System.Drawing.Size(125, 31);
+            this.ssData.TabIndex = 172;
+            this.ssData.TabStop = false;
+            this.ssData.Value = "0000";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(652, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 23);
+            this.button1.TabIndex = 183;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(672, 583);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.zed1);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.ssData);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private SevenSegmentArray ssData;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private ZedGraph.ZedGraphControl zed1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace VectonSensingDemo2
+{
+    public partial class Form1 : Form
+    {
+        Thread m_thread;
+        bool m_running = false;
+        ManualResetEvent m_event = new ManualResetEvent(true);
+        bool m_pause = false;
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            CreateZedGraph();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+            if (m_thread == null || m_thread.IsAlive == false)
+            {
+                ClearGraph();
+                m_thread = new Thread(Process);
+                m_thread.Start();
+            }
+        }
+        void Process()
+        {
+            Random random = new Random();
+            int index = 0;
+            PointPair point = new PointPair();
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            m_running = true;
+            while (m_running == true)
+            {
+                m_event.WaitOne();
+                point.Y = random.Next(0, 1000);
+                point.X++;
+                DrawPoint(zed1, index, point);
+                ssData.Value = point.Y.ToString();
+                RefresheZedGraphs(zed1);
+                Thread.Sleep(700);                
+            }
+            btnStart.Enabled = true;
+        }
+        private void CreateZedGraph()
+        {
+            Random rand = new Random();
+            PointPairList spl3 = new PointPairList();
+            Color c1 = Color.FromArgb(255, rand.Next(255), rand.Next(255), rand.Next(255));
+            zed1.GraphPane.AddCurve("Demo", spl3, c1, SymbolType.None);
+        }
+        void ClearGraph()
+        {
+
+            zed1.GraphPane.CurveList[0].Clear();           
+         
+            RefresheZedGraphs(zed1);
+           
+        }
+        private void DrawPoint(ZedGraph.ZedGraphControl zgc, int index, PointPair p)
+        {
+            zgc.GraphPane.CurveList[index].AddPoint(p);
+        }
+        
+        private void RefresheZedGraphs(ZedGraph.ZedGraphControl zg)
+        {
+            zg.AxisChange();
+            zg.Invalidate();
+            //zg.Refresh();
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            m_running = false;
+            m_event.Set();
+            if (m_thread == null)
+                m_thread.Join();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (m_pause == false)
+            {
+                m_event.Reset();
+            }
+            else
+            {
+                m_event.Set();
+            }
+            m_pause = !m_pause;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            m_event.Set();
+            m_running = false;
+            if (m_thread != null)
+                m_thread.Join();
+            Close();
+        }
+    }
+}
